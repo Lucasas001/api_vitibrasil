aa1ef1e78dfe2717361206792e5ddd67ec6c98e4
iamthehype <lucasec911@gmail.com>
2024-11-01 17:10:54 -0300

    hotfix da formatação do arquivo, troca de CRLF para FL

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,9 @@
 
 # Inicia o cron em segundo plano
 cron && tail -f /var/log/cron.log &
-
 echo "Executa o script csv_downloader.py na inicialização"
-poetry run python api_vitibrasil/modules/csv_downloader.py
-
+poetry run python3 /app/api_vitibrasil/modules/csv_downloader.py
 echo "Executa o script csv_processor.py na inicialização"
-poetry run python api_vitibrasil/modules/csv_processor.py
-
+poetry run python3 /app/api_vitibrasil/modules/csv_processor.py
 echo "Scripts finalizados, iniciando API Service"
 poetry run start
